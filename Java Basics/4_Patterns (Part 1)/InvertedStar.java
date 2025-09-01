54aefbab75a273a6ef3c29bdd50e8538747be072
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-09-02 00:57:37 +0530

    01 Sept

---
+++ Java Basics/4_Patterns (Part 1)/InvertedStar.java
@@ -0,0 +1,10 @@
+public class InvertedStar {
+    public static void main(String args[]){
+        for(int line=7; line>=1; line--){
+            for(int star=1; star<=line; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }    
+}
