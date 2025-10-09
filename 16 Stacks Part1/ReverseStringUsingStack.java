a093093f91d9c3e76591e17582f908b5a15af713
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-10-09 22:51:37 +0530

    09 Oct

--- 16 Stacks Part1/ReverseStringUsingStack.java
+++ 16 Stacks Part1/ReverseStringUsingStack.java
@@ -1,7 +1,28 @@
-
+import java.util.*;
 
 public class ReverseStringUsingStack {
+    public static String reverseString(String str){
+        Stack<Character> stack = new Stack<>();
+        int index = 0;
+        while (index < str.length()) {
+            stack.push(str.charAt(index));
+            index++;
+        }
+
+        StringBuilder sb = new StringBuilder("");
+
+        while (!stack.isEmpty()) {
+            char top = stack.peek();
+            sb.append(top);
+            stack.pop();
+        }
+
+        str = sb.toString();
+        return str;
+    }
+
     public static void main(String[] args) {
-        
+        String str = "abc";
+        System.out.println(reverseString(str));
     }
 }
