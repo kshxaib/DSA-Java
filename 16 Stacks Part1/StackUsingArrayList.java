14103b0da143016c4d38cb523d0c4cb143aa7d4e
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-10-09 21:50:52 +0530

    09 Oct

---
+++ 16 Stacks Part1/StackUsingArrayList.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class StackUsingArrayList {
+    public static class Stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+        public static boolean isEmpty(){
+            return list.size() == 0;
+        }
+
+        public static void push(int data){
+            list.add(data);
+        }
+
+        public static int pop(){
+            int top = list.get(list.size() -1);
+            list.remove(list.size() -1);
+            return top;
+        }
+
+        public static int peek(){
+            int top = list.get(list.size() -1);
+            return top;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+
+        while(!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
