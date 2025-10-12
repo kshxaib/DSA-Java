d32922a5549e84caad44a37a996aa405fd7bd2e1
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-10-12 21:44:38 +0530

    12 Oct

---
+++ 18 Queues/QueueUsingDeque.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.util.LinkedList;
+
+public class QueueUsingDeque {
+    static class Queue{
+        Deque<Integer> deque = new LinkedList<>();
+
+        public boolean isEmpty() {
+            return deque.isEmpty();
+        }
+
+        public void add(int data) {
+            deque.addLast(data); 
+        }
+
+        public int remove() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty, cannot remove.");
+                return -1;
+            }
+            return deque.removeFirst();
+        }
+
+        public int front() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty, cannot peek.");
+                return -1;
+            }
+            return deque.getFirst(); 
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+
+        while(!queue.isEmpty()){
+            System.out.println(queue.remove());
+        }
+    }
+}
