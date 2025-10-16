6c73133260b372222e0eb8ecd80edb4728b0d806
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-10-16 15:25:21 +0530

    16 Oct

--- 19 Greedy Algorithms/MinAbsoluteDifferenceSum.java
+++ 19 Greedy Algorithms/MinAbsoluteDifferenceSum.java
@@ -1,3 +1,23 @@
+import java.util.*;
+
 public class MinAbsoluteDifferenceSum {
-    
+    public static int findMinAbsDiffSum(int A[], int B[]){
+        int minSum = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for(int i=0; i<A.length; i++){
+            minSum = minSum + Math.abs((A[i] - B[i]));
+        }
+
+        return minSum;
+    }
+
+    public static void main(String[] args) {
+        int A[] = {1,2,3};
+        int B[] = {2,1,3};
+
+        System.out.println(findMinAbsDiffSum(A, B));
+    }
 }
