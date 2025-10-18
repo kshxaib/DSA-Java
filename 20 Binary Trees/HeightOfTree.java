6e7c7d166b3a5d000b5ae422d5a37487a019bc5b
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-10-18 12:35:53 +0530

    18 Oct

---
+++ 20 Binary Trees/HeightOfTree.java
@@ -0,0 +1,37 @@
+public class HeightOfTree {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int findHeight(Node root){
+        if(root == null){
+            return 0;
+        }
+
+        int leftHeight = findHeight(root.left);
+        int rightHeight = findHeight(root.right);
+
+        int height = Math.max(leftHeight, rightHeight) +1;
+        return height;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(findHeight(root));
+    }
+}
