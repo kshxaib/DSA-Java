b65756e46404e8bdfffab310ce116931a6e82539
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-10-20 16:57:44 +0530

    20 Oct

---
+++ 23 Binary Search Trees Part1/RootToLeafPaths.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class RootToLeafPaths {
+    static class Node {
+        int data;       // Value of the node
+        Node left;      // Reference to left child
+        Node right;     // Reference to right child
+
+        // Constructor: initializes node value and sets children to null
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // Step 2: Recursive function to insert a value into BST
+    public static Node insert(Node root, int val) {
+        // Base Case: If tree is empty, create a new node
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+
+        // If value is smaller, insert into left subtree
+        if (root.data > val) {
+            root.left = insert(root.left, val);
+        } 
+        // If value is larger or equal, insert into right subtree
+        else {
+            root.right = insert(root.right, val);
+        }
+
+        // Return unchanged root node
+        return root;
+    }
+    
+    public static void printPaths(Node root, ArrayList<Integer> path){
+        if(root == null){
+            return;
+        }
+
+        path.add(root.data);
+
+        if(root.left == null && root.right == null){
+            print(path);
+            System.out.println();
+        }
+
+        printPaths(root.left, path);
+        printPaths(root.right, path);
+
+        path.remove(path.size() -1);
+    }
+
+    public static void print(ArrayList<Integer> path){
+        for(int i=0; i<path.size(); i++){
+            System.out.print(path.get(i) + " ");
+        }
+    }
+    
+    public static void main(String[] args) {
+        int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};   // Values to insert into BST
+        Node root = null;                                // Initially empty tree
+
+        // Insert values one by one into BST
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);              // Assign returned root each time
+        }
+
+        printPaths(root, new ArrayList<>());
+    }
+}
