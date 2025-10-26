17d00f9206f21cadc9adede5974ceb9cf0e1ce7c
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-10-26 23:36:12 +0530

    26 Oct

--- 6 Bit Manipulation/updateIthBit.java
+++ 6 Bit Manipulation/updateIthBit.java
@@ -1,15 +1,14 @@
 public class updateIthBit {
     // Function to update ith bit of number n to newValueOfBit (0 or 1)
     public static int updateIthBitValue(int n, int i, int newValueOfBit){
-        // Step 1: First clear the ith bit (make it 0)
-        int bitMask = ~(1 << i);
-        n = n & bitMask;
-
-        // Step 2: Set the ith bit to newValueOfBit
-        // If newValueOfBit = 1 → OR operation will set ith bit to 1
-        // If newValueOfBit = 0 → OR with 0 keeps ith bit 0
-        return n | (newValueOfBit << i);
-    }
+       if(newValueOfBit == 0){
+           int bitMask = ~(1 << i);
+           return n & bitMask;
+       } else {
+           int bitMask = 1 << i;
+           return n | bitMask;
+       }
+    }   
 
     public static void main(String[] args) {
         int n = 10;  // binary: 1010
