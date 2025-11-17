04f596c7b3727ab1ecc43db3edcf1bd316fec0e8
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-11-18 00:21:49 +0530

    18 Nov

--- 25_Heaps/NearbyCars.java
+++ 25_Heaps/NearbyCars.java
@@ -2,21 +2,19 @@ import java.util.PriorityQueue;
 
 public class NearbyCars {
 
-    // Class representing a point (car) with distance and index
     static class Point implements Comparable<Point> {
         int x;
         int y;
-        int distSq;   // distance squared from origin (x^2 + y^2)
-        int index;    // original index of the point in the input array
+        int distSq;  
+        int index;   
 
         public Point(int x, int y, int distSq, int i){
             this.x = x;
             this.y = y;
             this.distSq = distSq;
-            this.index = i;     // store the index correctly
+            this.index = i;    
         }
 
-        // Min-heap comparison based on distance
         @Override
         public int compareTo(Point p2){
             return this.distSq - p2.distSq;
@@ -28,18 +26,15 @@ public class NearbyCars {
         int points[][] = {{3,3}, {5,-1}, {-2,4}}; 
         int k = 2;
 
-        // Min-heap storing cars ordered by distance from origin
         PriorityQueue<Point> pq = new PriorityQueue<>();
 
-        // Insert all cars into the priority queue
         for(int i = 0; i < points.length; i++){
             int x = points[i][0];
             int y = points[i][1];
-            int distSq = x*x + y*y;  // compute squared distance
+            int distSq = x*x + y*y;  
             pq.add(new Point(x, y, distSq, i));
         }
 
-        // Remove k nearest cars
         for(int i = 0; i < k; i++){
             System.out.println("C" + pq.remove().index);
         }
