59e54a34ba32197692bfbba34df3606826c08dab
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-11-28 14:32:55 +0530

    28 Nov

--- 26 Hashing/LargsetSubarrayWith0Sum.java
+++ 26 Hashing/LargsetSubarrayWith0Sum.java
@@ -1,3 +1,29 @@
+import java.util.HashMap;
+
 public class LargsetSubarrayWith0Sum {
-    
+    public static int findLength(int arr[]){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int len = 0;
+        int sum = 0;
+
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i];
+
+            if(map.containsKey(sum)){
+                int newLen = i - map.get(sum);
+                if(newLen > len){
+                    len = newLen;
+                }
+            } else {
+                map.put(sum, i);
+            }
+        }
+
+        return len;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {15, -2, 2, -8, 1, 7, 10};
+        System.out.println(findLength(arr));
+    }    
 }
