188fd445ae6522064a48ea0ed37d0a612f890b9f
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-11-28 22:05:00 +0530

    28 Nov

---
+++ 27 Tries/ImplementationOfTrie.java
@@ -0,0 +1,18 @@
+public class ImplementationOfTrie{
+    static class Node{
+        Node children[] = new Node[26];
+        boolean endOfNode = false;
+
+        Node(){
+            for(int i=0; i<26; i++){
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void main(String[] args) {
+        String words[] = {"the", "a", "there", "their", "any", "thee"};
+    }
+}
