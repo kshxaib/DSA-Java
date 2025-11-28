a6e97c2cb16e5bf44c7aef908004de25a25ef772
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-11-28 10:37:29 +0530

    28 Nov

--- 26 Hashing/LinkedHashSetExample.java
+++ 26 Hashing/LinkedHashSetExample.java
@@ -1,3 +1,13 @@
+import java.util.*;
+
 public class LinkedHashSetExample {
-    
+    public static void main(String[] args) {
+        LinkedHashSet<String> cities = new LinkedHashSet<>();
+
+        cities.add("Delhi");
+        cities.add("Mumbai");
+        cities.add("Bangalore");
+
+        System.out.println(cities);
+    }
 }
