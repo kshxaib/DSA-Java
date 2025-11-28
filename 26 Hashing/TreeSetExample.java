6a3d2337f7fbb5598055ed6b1d86febf28b0009e
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-11-28 10:50:00 +0530

    28 Nov

---
+++ 26 Hashing/TreeSetExample.java
@@ -0,0 +1,13 @@
+import java.util.TreeSet;
+
+public class TreeSetExample {
+    public static void main(String[] args) {
+        TreeSet<Integer> ts = new TreeSet<>();
+
+        ts.add(3);
+        ts.add(1);
+        ts.add(2);
+
+        System.out.println(ts);
+    }
+}
