238e3c0e0c2a51fa4213baea2577ef0516161d9a
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-11-28 23:30:17 +0530

    28 Nov

--- 27 Tries/ImplementationOfTrie.java
+++ 27 Tries/ImplementationOfTrie.java
@@ -1,7 +1,7 @@
 public class ImplementationOfTrie{
     static class Node{
         Node children[] = new Node[26];
-        boolean endOfNode = false;
+        boolean endOfWord = false;
 
         Node(){
             for(int i=0; i<26; i++){
@@ -12,7 +12,23 @@ public class ImplementationOfTrie{
 
     public static Node root = new Node();
 
+    public static void insert(String word){
+        Node curr = root;
+        for(int lvl=0; lvl<word.length(); lvl++){
+            int idx = word.charAt(lvl) - 'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node(); 
+            }
+            curr = curr.children[idx];
+        }
+
+        curr.endOfWord = true;
+    }
+
     public static void main(String[] args) {
         String words[] = {"the", "a", "there", "their", "any", "thee"};
+        for(int i=0; i<words.length; i++){
+            insert(words[i]);
+        }
     }
 }
