3dcbf314169b4c3ca1877863c06f0d9c63c323c4
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-11-29 13:33:24 +0530

    29 Nov

--- 27 Tries/ImplementationOfTrie.java
+++ 27 Tries/ImplementationOfTrie.java
@@ -38,12 +38,39 @@ public class ImplementationOfTrie{
         return curr.endOfWord;
     }
 
-    public static void main(String[] args) {
-        String words[] = {"the", "a", "there", "their", "any", "thee"};
-        for(int i=0; i<words.length; i++){
-            insert(words[i]);
+    public static boolean wordBreak(String key) {   // O(L)
+        if (key.length() == 0) {
+            return true;
         }
 
-        System.out.println(search("their"));
+        for (int i = 1; i <= key.length(); i++) {
+
+            // key.substring(0, i) = prefix
+            // key.substring(i)    = remaining suffix
+            if (search(key.substring(0, i))     
+                    && wordBreak(key.substring(i))) {  
+                return true;
+            }
+        }
+
+        return false;
     }
-}
+
+    public static void main(String[] args) {
+        // Implementation of Trie
+        // String words[] = {"the", "a", "there", "their", "any", "thee"};
+        // for(int i=0; i<words.length; i++){
+        //     insert(words[i]);
+        // }
+
+        // System.out.println(search("their"));
+
+        // Word Breaking Problem
+        String arr[] = {"i", "like", "sam", "samsung", "mobile", "ice"};
+        for(int i=0; i<arr.length; i++){
+            insert(arr[i]);
+        }
+        String key = "ilikesamsung";
+        System.out.println(wordBreak(key));
+    }
+}`
