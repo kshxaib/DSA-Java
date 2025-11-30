87f77efbc1231a8a6ad3a2814939e2c5da417614
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-11-30 13:20:24 +0530

    30 Nov

--- 27 Tries/ImplementationOfTrie.java
+++ 27 Tries/ImplementationOfTrie.java
@@ -56,21 +56,78 @@ public class ImplementationOfTrie{
         return false;
     }
 
+
+    //---------------Prefix Problem--------------------
+    static class Node2{
+        Node2 children [] = new Node2[26];
+        boolean endOfWord = false;
+        int freq;
+
+        public Node2(){
+            for(int i=0; i<26; i++){
+                children[i] = null;
+            }
+            freq = 1;
+        }
+    }
+
+    public static Node2 root2 = new Node2();
+
+    public static void insert2(String word){
+        Node2 curr = root2;
+        for(int lvl=0; lvl<word.length(); lvl++){
+            int idx = word.charAt(lvl) - 'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node2();
+            } else{
+                curr.children[idx].freq++;
+            }
+
+            curr = curr.children[idx];
+        }
+
+        curr.endOfWord = true;
+    }
+
+    public static void findPrefix(Node2 root2, String ans){
+        if(root2 == null){
+            return;
+        }
+
+        if(root2.freq == 1){
+            System.out.println(ans);
+            return;
+        }
+
+        for(int i=0; i<26; i++){
+            if(root2.children[i] != null){
+                findPrefix(root2.children[i], ans+(char)(i+'a'));
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // Implementation of Trie
         // String words[] = {"the", "a", "there", "their", "any", "thee"};
         // for(int i=0; i<words.length; i++){
         //     insert(words[i]);
         // }
-
         // System.out.println(search("their"));
 
         // Word Breaking Problem
-        String arr[] = {"i", "like", "sam", "samsung", "mobile", "ice"};
+        // String arr[] = {"i", "like", "sam", "samsung", "mobile", "ice"};
+        // for(int i=0; i<arr.length; i++){
+        //     insert(arr[i]);
+        // }
+        // String key = "ilikesamsung";
+        // System.out.println(wordBreak(key));
+
+        // Prefix Problem
+        String arr[] = {"zebra", "dog", "duck", "dove"};
         for(int i=0; i<arr.length; i++){
-            insert(arr[i]);
+             insert(arr[i]);
         }
-        String key = "ilikesamsung";
-        System.out.println(wordBreak(key));
+        root2.freq = -1;
+        findPrefix(root2, "");
     }
 }
