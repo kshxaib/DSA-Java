7bd9ce83835cfbad2e90dffb545e64e34d5d1166
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-12-17 14:15:31 +0530

    17 Dec

---
+++ 31 Graphs Part4/PrimsAlgo.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.util.LinkedList;
+
+public class PrimsAlgo {
+    static class Edge {
+        int src;
+        int dest;
+        int wgt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wgt = w;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 1, 10));
+        graph[0].add(new Edge(0, 2, 15));
+        graph[0].add(new Edge(0, 3, 30));
+
+        graph[1].add(new Edge(1, 0, 10));
+        graph[1].add(new Edge(1, 3, 40));
+
+        graph[2].add(new Edge(2, 0, 15));
+        graph[2].add(new Edge(2, 3, 50));
+
+        graph[3].add(new Edge(3, 1, 40));
+        graph[3].add(new Edge(3, 2, 50));
+    }
+
+    static class Pair implements Comparable<Pair>{
+        int vertex;
+        int cost;
+
+        public Pair(int v, int c){
+            this.vertex = v;
+            this.cost = c;
+        }
+
+        @Override
+        public int compareTo(Pair p2){
+            return this.cost - p2.cost;
+        }
+    }
+
+    public static void prims(ArrayList<Edge> graph[]){
+        boolean vis[] = new boolean[graph.length];
+        PriorityQueue<Pair> pq = new PriorityQueue<>(); 
+
+        pq.add(new Pair(0, 0));
+        int finalCost = 0;
+
+        while(!pq.isEmpty()){
+            Pair curr = pq.remove();
+
+            if(!vis[curr.vertex]){
+                vis[curr.vertex] = true;
+                finalCost = finalCost + curr.cost;
+
+                for(int i=0; i<graph[curr.vertex].size(); i++){
+                    Edge e = graph[curr.vertex].get(i);
+                    pq.add(new Pair(e.dest, e.wgt));
+                }
+            }
+        }
+
+        System.out.print("final cost of MST: " + finalCost);
+    }
+
+    public static void main(String[] args) {
+        int V = 4;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+
+        prims(graph);
+    }
+}
+
+/*
+Time Complexity: O(V * E)
+- Outer loop runs V-1 times
+- Inner loops traverse all edges
+
+Space Complexity: O(V)
+- dist[] array
+*/
