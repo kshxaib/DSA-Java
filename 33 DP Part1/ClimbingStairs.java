1e900659824767a445bf642e3c1bc8cffb314c74
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-12-29 00:40:23 +0530

    29 Dec

--- 33 DP Part1/ClimbingStairs.java
+++ 33 DP Part1/ClimbingStairs.java
@@ -1,15 +1,26 @@
 public class ClimbingStairs {
-    public static int countWays(int n){
+    public static int countWays(int n){  //Time: O(2^n)
         if(n == 0) return 1;
         if(n < 0) return 0;
 
         return countWays(n-1) + countWays(n-2);
     }
 
+    public static int cW2(int n, int dp[]){   
+        if(n == 0) return 1;
+        if(n < 0) return 0;
+
+        if(dp[n] != 0){
+            return dp[n];
+        }
+
+        dp[n] = cW2(n-1, dp) + cW2(n-2, dp);
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         int n = 5;
-        System.out.println(countWays(n));
+        int dp[] = new int[n+1];
+        System.out.println(cW2(n, dp));
     }
 }
-
-// Time: O(2^n)
