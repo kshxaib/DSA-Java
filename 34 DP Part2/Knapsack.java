a684be93eda9e028db6c094a832332ff291563eb
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-12-29 10:46:21 +0530

    29 Dec

---
+++ 34 DP Part2/Knapsack.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Knapsack {
+    public static int knapsack(int val[], int wt[], int i, int W){
+        if(W == 0 || i == 0){
+            return 0;
+        }
+
+        if(wt[i-1] <= W){
+            int ans1 = val[i-1] + knapsack(val, wt, i-1, W-wt[i-1]);
+            int ans2 = knapsack(val, wt, i-1, W);
+            return Math.max(ans1, ans2);
+        } else{
+            return knapsack(val, wt, i-1, W);
+        }
+    }
+
+    public static void main(String[] args) {
+        int val[] = {15, 14, 10, 45, 30};
+        int wt[] = {2, 5, 1, 3, 4};
+        int W = 7;
+
+        System.out.println(knapsack(val, wt, val.length, W));
+    }    
+}
