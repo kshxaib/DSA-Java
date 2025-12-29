3f4da0348bfd01ed66c1d2d5a3c6bb9de00522d5
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-12-29 08:20:10 +0530

    29 Dec

--- 33 DP Part1/ClimbingStairs.java
+++ 33 DP Part1/ClimbingStairs.java
@@ -19,9 +19,25 @@ public class ClimbingStairs {
         return dp[n];
     }
 
+    //Tabulation
+    public static int cW3(int n){   //Time: O(n)
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+
+        for(int i=1; i<=n; i++){
+            if(i == 1){
+                dp[i] = dp[i-1];
+            } else {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         int n = 5;
         int dp[] = new int[n+1];
-        System.out.println(cW2(n, dp));
+        System.out.println(cW3(n));
     }
 }
