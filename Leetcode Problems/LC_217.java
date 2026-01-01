11e4543502589b2753d8403084e249149e00f82d
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-01-01 18:58:47 +0530

    01 Jan

---
+++ Leetcode Problems/LC_217.java
@@ -0,0 +1,11 @@
+public class LC_217 {
+    public boolean containsDuplicate(int[] nums) {
+       HashSet<Integer> set = new HashSet<>();
+
+       for(int i=0; i<nums.length; i++){
+        if(set.contains(nums[i])) return true;
+        else set.add(nums[i]);
+       } 
+       return false;
+    }
+}
