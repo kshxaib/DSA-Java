18142fab1ca799f894ee73470a43614925144443
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-01-02 12:29:47 +0530

    02 Jan

---
+++ Leetcode Problems/LC_242.java
@@ -0,0 +1,24 @@
+public class LC_242{
+    public boolean isAnagram(String s, String t) {
+      if(s.length() != t.length()){
+        return false;
+      }  
+
+      int freq[] = new int[26];
+      for(int i=0; i<s.length(); i++){
+        freq[s.charAt(i) - 'a']++;
+        freq[s.charAt(i) - 'a']--;
+      }
+
+      for(int i=0; i<freq.length; i++){
+        if(freq[i] != 0){
+            return false;
+        }
+      }
+
+      return true;
+    }
+}
+
+//Time: O(n)
+//Space: O(1)
