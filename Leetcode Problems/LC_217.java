a31171f75e9f00f9ca427aa0f09dec33eecd009d
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-01-03 08:57:33 +0530

    03 Jan

--- Leetcode Problems/LC_217.java
+++ Leetcode Problems/LC_217.java
@@ -1,11 +1,28 @@
-public class LC_217 {
-    public boolean containsDuplicate(int[] nums) {
-       HashSet<Integer> set = new HashSet<>();
+import java.util.HashSet;
 
-       for(int i=0; i<nums.length; i++){
-        if(set.contains(nums[i])) return true;
-        else set.add(nums[i]);
-       } 
-       return false;
+public class LC_217 {
+
+    // Checks if the array contains any duplicate element
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+    public boolean containsDuplicate(int[] nums) {
+
+        // HashSet to store unique elements
+        HashSet<Integer> set = new HashSet<>();
+
+        // Traverse the array
+        for (int i = 0; i < nums.length; i++) {
+
+            // If element already exists in set, duplicate found
+            if (set.contains(nums[i])) {
+                return true;
+            }
+
+            // Otherwise, add element to the set
+            set.add(nums[i]);
+        }
+
+        // No duplicates found
+        return false;
     }
 }
--- Leetcode Problems/LC_242.java
+++ Leetcode Problems/LC_242.java
@@ -1,24 +1,33 @@
-public class LC_242{
+public class LC_242 {
+
+    // Checks whether two strings are anagrams
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
     public boolean isAnagram(String s, String t) {
-      if(s.length() != t.length()){
-        return false;
-      }  
 
-      int freq[] = new int[26];
-      for(int i=0; i<s.length(); i++){
-        freq[s.charAt(i) - 'a']++;
-        freq[s.charAt(i) - 'a']--;
-      }
-
-      for(int i=0; i<freq.length; i++){
-        if(freq[i] != 0){
+        // If lengths are different, they cannot be anagrams
+        if (s.length() != t.length()) {
             return false;
         }
-      }
 
-      return true;
+        // Frequency array for 26 lowercase English letters
+        int freq[] = new int[26];
+
+        // Increase frequency for characters in s
+        // Decrease frequency for characters in t
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+
+        // If any frequency is not zero, strings are not anagrams
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
+        }
+
+        // All frequencies matched
+        return true;
     }
 }
-
-//Time: O(n)
-//Space: O(1)
