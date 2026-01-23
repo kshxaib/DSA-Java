8e1b59a8041840b9cf40ba559318d4fdb9d3e44f
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-01-23 16:58:48 +0530

    23 Jan

--- 35 DP Part3/LCS.java
+++ 35 DP Part3/LCS.java
@@ -38,12 +38,41 @@ public class LCS {
         return Math.max(ans1, ans2);
     }
 
+    public static int lcsM(String str1, String str2, int n, int m, int dp[][]){
+        if (n == 0 || m == 0) {
+            return 0;
+        }
+
+        if (dp[n][m] != -1) {
+            return dp[n][m];
+        }
+
+        if (str1.charAt(n - 1) == str2.charAt(m - 1)) {
+            return dp[n][m] = lcsM(str1, str2, n - 1, m - 1, dp) + 1;
+        }
+
+        int ans1 = lcsM(str1, str2, n - 1, m, dp);
+        int ans2 = lcsM(str1, str2, n, m - 1, dp);
+
+        return dp[n][m] = Math.max(ans1, ans2);
+    }
+
     public static void main(String[] args) {
 
         String str1 = "abcdge";
         String str2 = "abedg";
+        int n = str1.length();
+        int m = str2.length();
 
-        System.out.println(lcs(str1, str2, str1.length(), str2.length()));
+        System.out.println(lcs(str1, str2, n, m));
+
+        int dp[][] = new int[n+1][m+1];
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<m+1; j++){
+                dp[i][j] = -1;
+            }
+        }
+        System.out.println(lcsM(str1, str2, n, m, dp));
     }
 }
 
