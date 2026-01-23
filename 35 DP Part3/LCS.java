eb341623c4e4989700bacea1fa6986a8f2d7ebba
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-01-23 17:41:25 +0530

    23 Jan

--- 35 DP Part3/LCS.java
+++ 35 DP Part3/LCS.java
@@ -76,6 +76,31 @@ public class LCS {
         return dp[n][m];
     }
 
+    public static int lcsT(String str1, String str2, int n, int m){
+        int dp[][] = new int[n+1][m+1];
+
+        for(int i=0; i<n+1; i++){
+            dp[i][0] = 0;
+        }
+        for(int j=0; j<m+1; j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<m+1; j++){
+                if(str1.charAt(i-1) == str2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                } else {
+                    int ans1 = dp[i-1][j];
+                    int ans2 = dp[i][j-1];
+                    dp[i][j] = Math.max(ans1, ans2);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
     public static void main(String[] args) {
 
         String str1 = "abcdge";
@@ -83,22 +108,21 @@ public class LCS {
 
         int n = str1.length();
         int m = str2.length();
-
         // Recursive answer
         System.out.println(lcs(str1, str2, n, m));
 
         // dp table for memoization
         int dp[][] = new int[n + 1][m + 1];
-
         // Initialize dp with -1 (means not computed yet)
         for (int i = 0; i < n + 1; i++) {
             for (int j = 0; j < m + 1; j++) {
                 dp[i][j] = -1;
             }
         }
-
         // Memoization answer
         System.out.println(lcsM(str1, str2, n, m, dp));
+
+        System.out.println(lcsT(str1, str2, n, m));
     }
 }
 
