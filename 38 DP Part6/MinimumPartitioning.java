9a32650fb94daace8de92233c0d1313270df8e44
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-01-24 23:11:58 +0530

    24 Jan

---
+++ 38 DP Part6/MinimumPartitioning.java
@@ -0,0 +1,33 @@
+public class MinimumPartitioning {
+    public static int MP(int arr[]){
+        int n = arr.length;
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+        }
+
+        int W = sum/2;
+
+        int dp[][] = new int[n+1][W+1];
+
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<W+1; j++){
+                int v = arr[i-1];
+                if(v <= j){
+                    dp[i][j] = Math.max(v + dp[i-1][j-v],dp[i-1][j]);
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        int sum1 = dp[n][W];
+        int sum2 = sum - sum1;
+        return Math.abs(sum1- sum2);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 6, 11, 5};
+        System.out.println(MP(arr));
+    }    
+}
