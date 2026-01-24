5039e3c7d111abaebea711f2c0ece59bfbcc4559
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-01-24 12:59:13 +0530

    24 Jan

--- 37 DP Part5/CatalanNumber.java
+++ 37 DP Part5/CatalanNumber.java
@@ -92,6 +92,20 @@ public class CatalanNumber {
         return dp[n] = ans;
     }
 
+    public static int CNT(int n){
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2; i<n+1; i++){
+            for(int j=0; j<i; j++){
+                dp[i] += dp[j] * dp[i-j-1]; 
+            }
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         int n = 4;
 
@@ -104,6 +118,8 @@ public class CatalanNumber {
 
         // Memoization answer
         System.out.println(CNM(n, dp));
+
+        System.out.println(CNT(n));
     }
 }
 
