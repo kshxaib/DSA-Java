4e878c4984918f9b5b835d74386b1497a1f0c50e
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-01-26 02:33:54 +0530

    26 Jan

--- 39 Segment Trees/SegmentTree.java
+++ 39 Segment Trees/SegmentTree.java
@@ -1,12 +1,11 @@
-public class CreationOfST {
+public class SegmentTree {
 
     // Segment Tree array (stores segment sums)
     static int tree[];
 
-    /*
-      Initialize Segment Tree
-      Size = 4*n (safe size to store complete tree)
-    */
+    
+    // Initialize Segment Tree
+    // Size = 4*n (safe size to store complete tree)
     public static void init(int n) {
         tree = new int[4 * n];
     }
@@ -45,6 +44,24 @@ public class CreationOfST {
         return tree[i];
     }
 
+    public static int getSumUtil(int i, int si, int sj, int qi, int qj){
+        if (qj <= si || qi >= sj){
+          return 0;
+        } else if (si >= qi && sj <= qj){
+          return tree[i];
+        } else {
+          int mid = (si + sj)/2;
+          int left = getSumUtil(2*i+1, si, mid, qi, qj);
+          int right = getSumUtil(2*i+2, mid+1, sj, qi, qj);
+          return left + right;
+        }
+    }
+
+    public static int getSum(int arr[], int qi, int qj){
+        int n = arr.length;
+        return getSumUtil(0, 0, n-1, qi, qj);
+    }
+
     public static void main(String[] args) {
 
         int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
@@ -52,6 +69,8 @@ public class CreationOfST {
 
         init(n);
         System.out.println(buildST(arr, 0, 0, n - 1)); // Total sum = 36
+
+        System.out.println(getSum(arr, 2, 5));
     }
 }
 
