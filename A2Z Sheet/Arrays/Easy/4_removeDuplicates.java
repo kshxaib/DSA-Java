ad420103999abe841cb3216fdeb1b26a135659c4
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-01-31 16:45:50 +0530

    31 Jan

---
+++ A2Z Sheet/Arrays/Easy/4_removeDuplicates.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=0;
+        for(int j=1; j<nums.length; i++){
+            if(nums[j] != nums[i]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i+1;
+    }
+}
+
+// Time: O(n)
+// Space: O(1)
