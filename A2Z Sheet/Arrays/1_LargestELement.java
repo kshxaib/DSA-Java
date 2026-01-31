879334a54dad560e3d5a7ce9c515178323d695bb
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-01-31 13:22:15 +0530

    31 Jan

---
+++ A2Z Sheet/Arrays/1_LargestELement.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int largestElement(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] > max){
+                max = nums[i];
+            }
+        }
+
+        return max;
+    }
+}
+
+// Time: O(n)
+// Space: O(1)
