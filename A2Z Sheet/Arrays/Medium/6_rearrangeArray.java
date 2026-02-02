628345f51022d99c510b53766f4dd319f1eb0487
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-02-02 12:55:17 +0530

    02 Feb

---
+++ A2Z Sheet/Arrays/Medium/6_rearrangeArray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int posIdx = 0, negIdx = 1;
+        int ans[] = new int[n];
+
+        for(int i=0; i<n; i++){
+            if(nums[i] < 0){
+                ans[negIdx] = nums[i];
+                negIdx += 2;
+            } else {
+                ans[posIdx] = nums[i];
+                posIdx += 2;
+            }
+        }
+
+        return ans;
+    }
+}
+
+// Time: O(n)
+// Space: O(1)
