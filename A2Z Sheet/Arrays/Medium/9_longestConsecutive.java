b6e5ca12e3e74123418638eca5d00930e126b801
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-02-03 20:02:51 +0530

    03 Feb

---
+++ A2Z Sheet/Arrays/Medium/9_longestConsecutive.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int n = nums.length;
+        
+        if(n == 0) return 0;
+
+        int longest = 1;
+        Set<Integer> set = new HashSet<>();
+
+        for(int i=0; i<n; i++){
+            set.add(nums[i]);
+        }
+
+        for(int num : set){
+            if(!set.contains(num -1)){
+                int count = 1;
+                int x = num;
+
+                while(set.contains(x + 1)){
+                    count++;
+                    x++;
+                }
+
+                longest = Math.max(longest, count);
+            }
+        }
+
+        return longest;
+    }
+}
+
+// Time: O(n)
+// Space: O(n)
