9f7f4ec8d580cdca41aaac0afe1f85838461fd98
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-02-13 21:36:29 +0530

    13 Feb

---
+++ A2Z Sheet/Binary Search/1D/9_searchInARotatedSortedArrayII.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean searchInARotatedSortedArrayII(int[] nums, int k) {
+        int start = 0;
+        int end = nums.length -1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(nums[mid] == k){
+                return true;
+            }
+
+            if(nums[start] == nums[mid] && nums[mid] == nums[end]){
+                start++;
+                end--;
+                continue;
+            }
+
+            if(nums[start] <= nums[mid]){
+                if(nums[start] <= k && k <= nums[mid]){
+                    end = mid -1;
+                } else {
+                    start = mid +1;
+                }
+            } else {
+                if(nums[start] <= k && k <= nums[end]){
+                    start = mid +1;
+                } else {
+                    end = mid -1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
