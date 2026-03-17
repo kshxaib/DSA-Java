b403110f32bca12090843a3fc8add8b2ed4e0477
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-03-18 02:43:36 +0530

    18 Mar

---
+++ A2Z Sheet/LinkedList/Medium of LL/2_reverseList.java
@@ -0,0 +1,18 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+
+            prev = curr;
+            curr = next;
+        }
+
+        head = prev;
+        return head;
+    }
+}
