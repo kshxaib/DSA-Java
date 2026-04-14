1fa6d48ace4f6016b9b8611284b979075ad17bad
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-04-14 17:28:29 +0530

    14 Mar

--- A2Z Sheet/Bit Manipulation/Learn/5_countSetBits.java
+++ A2Z Sheet/Bit Manipulation/Learn/5_countSetBits.java
@@ -1,3 +1,31 @@
+/*
+        COUNT SET BITS (NUMBER OF 1s IN BINARY)
+
+Definition:
+Given an integer n,
+return the number of set bits (1s)
+in its binary representation.
+
+
+Example
+
+Input:
+n = 5  → (101)
+
+Output:
+2
+
+
+Core Idea
+
+Check LAST BIT repeatedly
+and shift number to the right.
+
+
+Time Complexity: O(log n) (number of bits in n)
+Space Complexity: O(1)
+*/
+
 class Solution {
     public int countSetBits(int n) {
         int count = 0;
