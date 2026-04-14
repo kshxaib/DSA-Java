32f82d145675ccc16e4d86c97a589887cb272360
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-04-14 18:47:20 +0530

    14 Mar

--- A2Z Sheet/Bit Manipulation/Learn/6_divide.java
+++ A2Z Sheet/Bit Manipulation/Learn/6_divide.java
@@ -1,3 +1,53 @@
+/*
+        DIVIDE TWO INTEGERS (WITHOUT USING *, /, %)
+
+Definition:
+Given two integers dividend and divisor,
+divide them WITHOUT using multiplication,
+division, or modulus operators.
+
+Return the quotient after division.
+
+
+Example
+
+Input:
+dividend = 10, divisor = 3
+
+Output:
+3
+
+
+Input:
+dividend = 7, divisor = -3
+
+Output:
+-2
+
+
+Core Idea (BIT MANIPULATION + SUBTRACTION)
+
+Instead of subtracting divisor repeatedly:
+a = a - b - b - b ...
+
+We use:
+SHIFTING (fast doubling)
+
+
+Why This Works
+b << shift = b * (2^shift)
+
+So we subtract large chunks instead of one by one.
+
+
+Time Complexity: O(log N * log N)
+Outer loop runs log N times  
+Inner loop finds shift (log N)
+
+
+Space Complexity: O(1)
+*/
+
 class Solution {
     public int divide(int dividend, int divisor) {
         if(dividend == divisor) return 1;
@@ -15,10 +65,12 @@ class Solution {
         long b = Math.abs((long) divisor);
 
         int ans = 0;
+
         while(a >= b){
 
             int shift = 0;
-            while(a >= (b << (shift +1))){
+
+            while(a >= (b << (shift + 1))){
                 shift++;
             }
 
