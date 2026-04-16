22bb9d4e8edf9cde6e9936c4a4f6bc45cf960b43
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-04-16 13:01:13 +0530

    16 Mar

--- A2Z Sheet/Bit Manipulation/Interview/5_singleNumber.java
+++ A2Z Sheet/Bit Manipulation/Interview/5_singleNumber.java
@@ -1,3 +1,68 @@
+/*
+        SINGLE NUMBER II (TWO UNIQUE ELEMENTS)
+
+Definition:
+Given an array where:
+• Every element appears exactly twice
+• Only TWO elements appear once
+
+Return those two unique elements.
+
+
+Example
+
+Input:
+nums = [1,2,1,3,2,5]
+
+Output:
+[3,5]
+
+
+Core Idea (XOR + BIT PARTITION)
+Step 1:
+XOR all elements
+
+Result:
+xor = a ^ b
+
+(where a and b are the two unique numbers)
+
+
+Why This Works
+All duplicates cancel out:
+
+x ^ x = 0
+
+So only:
+a ^ b remains
+
+
+Step 2:
+Find a DIFFERENT BIT between a and b
+
+We find first set bit in xor
+
+This bit tells:
+a and b differ at this position
+
+
+Step 3:
+Divide numbers into 2 groups
+
+Group 1 → bit is SET  
+Group 2 → bit is NOT SET  
+
+
+Step 4:
+XOR each group separately
+
+Each group will give one unique number
+
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
 class Solution {    
     public int[] singleNumber(int[] nums) {        
         int xor = 0;
