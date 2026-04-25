7ae15788a6fb6d072c2b57196ba146af4adae460
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-04-25 12:58:41 +0530

    25 Apr

---
+++ A2Z Sheet/Heaps/Learning/3_isHeap.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isHeap(int[] nums) {
+        int n = nums.length;
+
+        for(int i = 0; i < n; i++) {
+
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if(left < n && nums[i] > nums[left]) {
+                return false;
+            }
+
+            if(right < n && nums[i] > nums[right]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isHeap(int[] nums) {
+        int n = nums.length;
+
+        for(int i = 0; i < (n -1)/2; i++) {
+
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if(left < n && nums[i] > nums[left]) {
+                return false;
+            }
+
+            if(right < n && nums[i] > nums[right]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
