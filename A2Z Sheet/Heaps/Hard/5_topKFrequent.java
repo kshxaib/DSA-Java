ecc484247db7f0f27902bc2fc519801ebf2d23d7
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-06-03 22:26:20 +0530

    03 June

--- A2Z Sheet/Heaps/Hard/5_topKFrequent.java
+++ A2Z Sheet/Heaps/Hard/5_topKFrequent.java
@@ -25,4 +25,38 @@ class Solution {
 
         return result;
     }
+
+
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            int num = nums[i];
+            freq.put(num, freq.getOrDefault(num, 0) +1);
+        }
+
+        ArrayList<Integer>[] buckets = new ArrayList[nums.length +1];
+        for(int key : freq.keySet()){
+            int frequency = freq.get(key);
+            if(buckets[frequency] == null){
+                buckets[frequency] = new ArrayList<>();
+            }
+
+            buckets[frequency].add(key);
+        }
+
+        int result[] = new int[k];
+        int count = 0;
+        for(int i=buckets.length -1; i>=0 && count < k; i--){
+            if(buckets[i] != null){
+                for(int num : buckets[i]){
+                    result[count] = num;
+                    count++;
+                    if(count == k) break;
+                }
+            }
+        }
+
+        return result;
+    }
 }
