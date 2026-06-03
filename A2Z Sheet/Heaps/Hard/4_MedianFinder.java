0e671a61388bbd11b97f5d71e3bdd0794b812737
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-06-03 21:31:25 +0530

    03 June

--- A2Z Sheet/Heaps/Hard/4_MedianFinder.java
+++ A2Z Sheet/Heaps/Hard/4_MedianFinder.java
@@ -22,11 +22,33 @@ class MedianFinder {
             return midean;
         }
     }
+
+
+
+    PriorityQueue<Integer> maxHeap;
+    PriorityQueue<Integer> minHeap;
+
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<>((a,b) -> b-a);
+        minHeap = new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        maxHeap.offer(num);
+
+        minHeap.offer(maxHeap.poll());
+
+        if(minHeap.size() > maxHeap.size()){
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if(maxHeap.size() == minHeap.size()){
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        }
+
+        return maxHeap.peek();
+    }
 }
 
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder obj = new MedianFinder();
- * obj.addNum(num);
- * double param_2 = obj.findMedian();
- */
