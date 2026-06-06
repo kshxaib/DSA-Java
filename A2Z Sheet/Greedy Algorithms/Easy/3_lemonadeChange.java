3436424ad6ae819b6ef102437adda4292ac7514f
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-06-06 22:23:22 +0530

    06 June

--- A2Z Sheet/Greedy Algorithms/Easy/3_lemonadeChange.java
+++ A2Z Sheet/Greedy Algorithms/Easy/3_lemonadeChange.java
@@ -1,3 +1,115 @@
+/*
+        LEMONADE CHANGE
+
+Definition:
+At a lemonade stand:
+
+Each lemonade costs $5.
+
+Customers pay using:
+
+• $5
+• $10
+• $20
+
+Initially:
+
+No money in cash register.
+
+Return true if we can provide
+correct change to every customer.
+
+Otherwise return false.
+
+
+Example 1
+
+Input:
+bills = [5,5,5,10,20]
+
+Output:
+true
+
+Explanation:
+
+Customer 1:
+Pays $5
+No change needed
+
+Register:
+5 = 1
+
+
+Customer 2:
+Pays $5
+
+Register:
+5 = 2
+
+
+Customer 3:
+Pays $5
+
+Register:
+5 = 3
+
+
+Customer 4:
+Pays $10
+
+Give one $5
+
+Register:
+5 = 2
+10 = 1
+
+
+Customer 5:
+Pays $20
+Need $15 change
+
+Give:
+$10 + $5
+
+Register:
+5 = 1
+10 = 0
+Possible ✔
+
+
+Example 2
+Input:
+bills = [5,5,10,10,20]
+
+Output:
+false
+
+Explanation:
+Need $15 change for $20
+Only two $10 bills available
+Cannot give exact change.
+
+
+Greedy Idea
+Always prefer:
+$10 + $5
+
+instead of
+$5 + $5 + $5
+for a $20 bill.
+
+Why?
+$5 bills are more valuable
+for future transactions.
+
+
+Time Complexity: O(n)
+Single traversal
+
+Space Complexity:cO(1)
+Only two counters
+*/
+
 class Solution {
     public boolean lemonadeChange(int[] bills) {
         int dollar5 = 0; 
@@ -6,23 +118,41 @@ class Solution {
         for(int i=0; i<bills.length; i++){
             int bill = bills[i];
 
+            // Customer pays $5
             if(bill == 5){
                 dollar5++;
-            } else if(bill == 10){
+            }
+
+            // Customer pays $10
+            else if(bill == 10){
+
+                // Receive one $10
                 dollar10++;
+
+                // Need to give one $5 as change
                 if(dollar5 > 0){
                     dollar5--;
                 } else {
                     return false;
                 }
+            }
 
-            } else {
+            // Customer pays $20
+            else {
+
+                // Prefer giving $10 + $5
                 if(dollar5 > 0 && dollar10 > 0){
                     dollar5--;
                     dollar10--;
-                } else if(dollar5 >= 3) {
+                }
+
+                // Otherwise give 3 × $5
+                else if(dollar5 >= 3){
                     dollar5 -= 3;
-                } else {
+                }
+
+                // Change not possible
+                else {
                     return false;
                 }
             }
