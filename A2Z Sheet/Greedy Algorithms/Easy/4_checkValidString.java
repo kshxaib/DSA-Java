22b05cf4bfe403a33dde26a7ffc14a46dbdf856f
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-06-07 00:12:27 +0530

    07 June

--- A2Z Sheet/Greedy Algorithms/Easy/4_checkValidString.java
+++ A2Z Sheet/Greedy Algorithms/Easy/4_checkValidString.java
@@ -22,4 +22,29 @@ class Solution {
         return check(s, index+1, count+1) || check(s, index+1, count-1) ||
         check(s, index+1, count);
     }
+
+
+    public boolean checkValidString(String s) {
+        int minOpen = 0;
+        int maxOpen = 0;
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '('){
+                minOpen++;
+                maxOpen++;
+            } else if(ch == ')'){
+                minOpen--;
+                maxOpen--;
+            } else {
+                minOpen--;
+                maxOpen++;
+            }
+
+            if(minOpen < 0) minOpen =0;
+            if(maxOpen < 0) return false;
+        }
+
+        return minOpen == 0;
+    }
 }
