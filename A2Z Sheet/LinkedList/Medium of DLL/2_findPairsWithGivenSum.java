11adc51f7b3768e77354fb7b936d44820a07b17d
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-06-26 14:17:19 +0530

    26 June

--- A2Z Sheet/LinkedList/Medium of DLL/2_findPairsWithGivenSum.java
+++ A2Z Sheet/LinkedList/Medium of DLL/2_findPairsWithGivenSum.java
@@ -1,4 +1,15 @@
+import java.util.*;
+
 class Solution {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode prev;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
     public List<List<Integer>> findPairsWithGivenSumBrute(ListNode head, int target) {
         List<List<Integer>> ans = new ArrayList<>();
 
@@ -19,6 +30,35 @@ class Solution {
             }
 
             temp1 = temp1.next;
+        }
+
+        return ans;
+    }
+
+   
+    public List<List<Integer>> findPairsWithGivenSum(ListNode head, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        ListNode tail = head;
+        while(tail.next != null){
+            tail = tail.next;
+        }
+
+        ListNode leftPointer = head;
+        ListNode rightPointer = tail;
+
+        while(leftPointer.val < rightPointer.val){
+            int sum = leftPointer.val + rightPointer.val;
+
+            if(sum == target){
+                ans.add(Arrays.asList(leftPointer.val, rightPointer.val));
+                leftPointer = leftPointer.next;
+                rightPointer = rightPointer.next;
+            } else if(sum < target) {
+                leftPointer = leftPointer.next;
+            } else {
+                rightPointer = rightPointer.prev;
+            }
         }
 
         return ans;
