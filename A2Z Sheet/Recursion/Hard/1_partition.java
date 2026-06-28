521f8d927acd1bd776f5698b300c6e7ef5f6d69f
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-06-29 01:50:58 +0530

    29 June

--- A2Z Sheet/Recursion/Hard/1_partition.java
+++ A2Z Sheet/Recursion/Hard/1_partition.java
@@ -69,8 +69,16 @@ isPalindrome:
 partition:
 → initializes recursion
 */
+import java.util.*;
 
 class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans = new ArrayList<>();
+        List<String> curr = new ArrayList<>();
+
+        findPartitions(s, ans, curr, 0);
+        return ans;
+    }
 
     public void findPartitions(String s, List<List<String>> ans, List<String> curr, int index){
         if(index == s.length()){
@@ -78,19 +86,11 @@ class Solution {
             return;
         }
 
-        for(int i = index; i < s.length(); i++){
-
-            // check palindrome
+        for(int i=index; i<s.length(); i++){
             if(isPalindrome(s, index, i)){
-
-                // pick substring
-                curr.add(s.substring(index, i + 1));
-
-                // recurse for remaining string
-                findPartitions(s, ans, curr, i + 1);
-
-                // backtrack
-                curr.remove(curr.size() - 1);
+                curr.add(s.substring(index, i+1));
+                findPartitions(s, ans, curr,  i+1);
+                curr.remove(curr.size() -1);
             }
         }
     }
@@ -100,17 +100,11 @@ class Solution {
             if(s.charAt(index) != s.charAt(i)){
                 return false;
             }
+
             index++;
             i--;
         }
+
         return true;
-    }
-
-    public List<List<String>> partition(String s) {
-        List<List<String>> ans = new ArrayList<>();
-        List<String> curr = new ArrayList<>();
-
-        findPartitions(s, ans, curr, 0);
-        return ans;
     }
 }
