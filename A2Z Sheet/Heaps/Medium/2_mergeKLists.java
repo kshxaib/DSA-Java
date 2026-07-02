c62d14330145169f8825191b0010e6db3b2930de
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-03 01:05:45 +0530

    03 July

--- A2Z Sheet/Heaps/Medium/2_mergeKLists.java
+++ A2Z Sheet/Heaps/Medium/2_mergeKLists.java
@@ -51,15 +51,32 @@ Space Complexity: O(N)
 import java.util.*;
 
 class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
     public ListNode mergeKListsBrute(ListNode[] lists) {
 
         ArrayList<Integer> arr = new ArrayList<>();
 
         // collect all values
-        for(int i = 0; i < lists.length; i++){
+        for (int i = 0; i < lists.length; i++) {
             ListNode temp = lists[i];
 
-            while(temp != null){
+            while (temp != null) {
                 arr.add(temp.val);
                 temp = temp.next;
             }
@@ -72,7 +89,7 @@ class Solution {
         ListNode dummy = new ListNode(-1);
         ListNode dTemp = dummy;
 
-        for(int i = 0; i < arr.size(); i++){
+        for (int i = 0; i < arr.size(); i++) {
             dTemp.next = new ListNode(arr.get(i));
             dTemp = dTemp.next;
         }
@@ -80,34 +97,33 @@ class Solution {
         return dummy.next;
     }
 
-
-/*
-=====================================================
-2. ITERATIVE MERGING
-=====================================================
-
-Merge lists one-by-one.
-
-Like:
-(((L1 + L2) + L3) + L4)
-
-
-Time Complexity
-Worst case: O(k * N)
-Can become expensive.
-
-Space Complexity: O(1)
-*/
+    /*
+     * =====================================================
+     * 2. ITERATIVE MERGING
+     * =====================================================
+     * 
+     * Merge lists one-by-one.
+     * 
+     * Like:
+     * (((L1 + L2) + L3) + L4)
+     * 
+     * 
+     * Time Complexity
+     * Worst case: O(k * N)
+     * Can become expensive.
+     * 
+     * Space Complexity: O(1)
+     */
 
     public ListNode mergeKListsBetter(ListNode[] lists) {
-        if(lists == null || lists.length == 0){
+        if (lists == null || lists.length == 0) {
             return null;
         }
 
         ListNode head = lists[0];
 
         // merge one-by-one
-        for(int i = 1; i < lists.length; i++){
+        for (int i = 1; i < lists.length; i++) {
             head = merge(head, lists[i]);
         }
 
@@ -119,12 +135,11 @@ Space Complexity: O(1)
         ListNode dummy = new ListNode(-1);
         ListNode temp = dummy;
 
-        while(firstL != null && secondL != null){
-            if(firstL.val <= secondL.val){
+        while (firstL != null && secondL != null) {
+            if (firstL.val <= secondL.val) {
                 temp.next = firstL;
                 firstL = firstL.next;
-            }
-            else{
+            } else {
                 temp.next = secondL;
                 secondL = secondL.next;
             }
@@ -133,67 +148,65 @@ Space Complexity: O(1)
         }
 
         // remaining nodes
-        if(firstL != null){
+        if (firstL != null) {
             temp.next = firstL;
         }
 
-        if(secondL != null){
+        if (secondL != null) {
             temp.next = secondL;
         }
 
         return dummy.next;
     }
 
-
-/*
-=====================================================
-3. OPTIMAL (MIN HEAP)
-=====================================================
-
-Core Idea:
-Always pick smallest current node
-among all lists.
-
-Use Min Heap.
-
-
-Why This Works
-
-Heap top always gives
-globally smallest node.
-
-
-Process
-1. Insert all list heads into heap
-2. Remove smallest node
-3. Add its next node into heap
-4. Repeat
-
-
-Time Complexity
-Each node inserted/removed once.
-
-O(N log k)
-
-BEST solution.
-
-Space Complexity: O(k)
-Heap stores at most k nodes.
-*/
+    /*
+     * =====================================================
+     * 3. OPTIMAL (MIN HEAP)
+     * =====================================================
+     * 
+     * Core Idea:
+     * Always pick smallest current node
+     * among all lists.
+     * 
+     * Use Min Heap.
+     * 
+     * 
+     * Why This Works
+     * 
+     * Heap top always gives
+     * globally smallest node.
+     * 
+     * 
+     * Process
+     * 1. Insert all list heads into heap
+     * 2. Remove smallest node
+     * 3. Add its next node into heap
+     * 4. Repeat
+     * 
+     * 
+     * Time Complexity
+     * Each node inserted/removed once.
+     * 
+     * O(N log k)
+     * 
+     * BEST solution.
+     * 
+     * Space Complexity: O(k)
+     * Heap stores at most k nodes.
+     */
     public ListNode mergeKLists(ListNode[] lists) {
 
-        if(lists == null || lists.length == 0){
+        if (lists == null || lists.length == 0) {
             return null;
         }
 
         // min heap based on node value
         PriorityQueue<ListNode> pq = new PriorityQueue<>(
-                (a,b) -> a.val - b.val
-            );
+                (a, b) -> a.val - b.val);
 
         // insert first node of every list
-        for(int i = 0; i < lists.length; i++){
-            if(lists[i] != null){
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] != null) {
                 pq.add(lists[i]);
             }
         }
@@ -201,14 +214,14 @@ Heap stores at most k nodes.
         ListNode dummy = new ListNode(-1);
         ListNode temp = dummy;
 
-        while(!pq.isEmpty()){
+        while (!pq.isEmpty()) {
             // smallest node
             ListNode minNode = pq.remove();
             temp.next = minNode;
             temp = temp.next;
 
             // push next node from same list
-            if(minNode.next != null){
+            if (minNode.next != null) {
                 pq.add(minNode.next);
             }
         }
