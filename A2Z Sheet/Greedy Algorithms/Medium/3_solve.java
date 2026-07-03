ac2e563da2f2271a50e0df5544d686cb63070cd1
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-03 23:07:56 +0530

    03 July

--- A2Z Sheet/Greedy Algorithms/Medium/3_solve.java
+++ A2Z Sheet/Greedy Algorithms/Medium/3_solve.java
@@ -2,28 +2,20 @@ import java.util.*;
 
 class Solution {
     public long solve(int[] bt) {
-
-        // Shortest jobs first
-        Arrays.sort(bt);
-
+        Arrays.sort(bt);    // Shortest jobs first
 
         long totalWaiting = 0;
-
-        // Current elapsed time
-        long currentTime = 0;
-
+        long currentTime = 0;   // Current elapsed time
 
         for (int i = 0; i < bt.length; i++) {
-
             // Current process waits till currentTime
             totalWaiting += currentTime;
-
 
             // Execute current process
             currentTime += bt[i];
         }
 
-
         return totalWaiting / bt.length;
     }
 }
+
