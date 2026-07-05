be0523213c3941954754391531ec3ef14d9332ac
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-05 16:45:05 +0530

    05 July

---
+++ A2Z Sheet/Binary Trees/Imp problems/3_diameterOfBinaryTree.java
@@ -0,0 +1,23 @@
+class Solution {
+    int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        calculateHeight(root);
+        return diameter;
+    }
+
+    private int calculateHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = calculateHeight(node.left);
+        int rightHeight = calculateHeight(node.right);
+
+        diameter = Math.max(diameter, leftHeight + rightHeight);
+
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
+
+
