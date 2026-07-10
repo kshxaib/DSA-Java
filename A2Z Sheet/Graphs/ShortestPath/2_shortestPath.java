c6392905bf0620b794ff0e1d1b8e7ce2ed222686
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-11 00:12:50 +0530

    11 July

--- A2Z Sheet/Graphs/ShortestPath/2_shortestPath.java
+++ A2Z Sheet/Graphs/ShortestPath/2_shortestPath.java
@@ -1,27 +1,27 @@
 import java.util.*;
 
 class Solution {
-  public int[] shortestPath(int N, int M, int[][] edges) {
+    public int[] shortestPath(int N, int M, int[][] edges) {
         List<List<int[]>> adj = new ArrayList<>();
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             adj.add(new ArrayList<>());
         }
 
         // build weighted DAG
-        for(int edge[] : edges){
+        for (int edge[] : edges) {
             int u = edge[0];
             int v = edge[1];
             int wt = edge[2];
 
-            adj.get(u).add(new int[]{v, wt});
+            adj.get(u).add(new int[] { v, wt });
         }
 
         boolean visited[] = new boolean[N];
         Stack<Integer> stack = new Stack<>();
 
-         // topo sort
-        for(int i=0; i<N; i++){
-            if(!visited[i]){
+        // topo sort
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
                 dfs(i, adj, visited, stack);
             }
         }
@@ -32,38 +32,43 @@ class Solution {
         // source
         dist[0] = 0;
 
+        // remove nodes before source in topo order
+        while (!stack.isEmpty() && stack.peek() != 0) {
+            stack.pop();
+        }
+
+
         // process nodes in topo order
-        while(!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             int node = stack.pop();
 
-            for(int neighbour[] : adj.get(node)){
+            for (int neighbour[] : adj.get(node)) {
                 int adjNode = neighbour[0];
                 int weight = neighbour[1];
 
-                if(dist[node] + weight < dist[adjNode]){
+                if (dist[node] + weight < dist[adjNode]) {
                     dist[adjNode] = dist[node] + weight;
                 }
             }
         }
 
         // unreachable nodes
-        for(int i=0; i<N; i++){
-            if(dist[i] == Integer.MAX_VALUE){
+        for (int i = 0; i < N; i++) {
+            if (dist[i] == Integer.MAX_VALUE) {
                 dist[i] = -1;
             }
         }
 
         return dist;
 
-  }
+    }
 
-  private void dfs(int node, List<List<int[]>> adj, boolean visited[], Stack<Integer> stack){
+    private void dfs(int node, List<List<int[]>> adj, boolean visited[], Stack<Integer> stack) {
         visited[node] = true;
 
-        for(int neighbour[] : adj.get(node)){
+        for (int neighbour[] : adj.get(node)) {
             int adjNode = neighbour[0];
-
-            if(!visited[adjNode]){
+            if (!visited[adjNode]) {
                 dfs(adjNode, adj, visited, stack);
             }
         }
