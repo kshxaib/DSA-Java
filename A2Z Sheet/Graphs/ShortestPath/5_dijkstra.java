f6c91a915aae5d90992b2a2ccaa5b47e2f963b17
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-11 12:23:39 +0530

    11 July

--- A2Z Sheet/Graphs/ShortestPath/5_dijkstra.java
+++ A2Z Sheet/Graphs/ShortestPath/5_dijkstra.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int[] dijkstra(int V, ArrayList<ArrayList<Integer>> edges, int S){
-        List<List<int[]>> adj = new ArrayList<>();
+        List<List<int[]>> adj = new ArrayList<>();  // adjacency list storing {node, weight}
         for(int i=0; i<V; i++){
             adj.add(new ArrayList<>());
         }
@@ -17,12 +17,12 @@ class Solution {
             adj.get(v).add(new int[]{u, wt});
         }
 
-        int dist[] = new int[V];
+        int dist[] = new int[V];     // stores shortest distance
         Arrays.fill(dist, (int)1e9);
 
-        dist[S] = 0;
+        dist[S] = 0;     // source distance
 
-        // {distance, node}
+        // min heap storing {distance,node}
         PriorityQueue<int[]> pq = new PriorityQueue<>(
             (a,b) -> a[0] - b[0]
         );
@@ -34,6 +34,7 @@ class Solution {
             int distance = pair[0];
             int node = pair[1];
 
+            // relaxation: found shorter path
             for(int neighbour[] : adj.get(node)){
                 int adjNode = neighbour[0];
                 int weight = neighbour[1];
