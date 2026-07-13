1ebc231a5c27d6251a2c2f911870eb0fdd4770d4
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-13 09:20:10 +0530

    13 July

--- A2Z Sheet/Strings/Hard/3_repeatedStringMatch.java
+++ A2Z Sheet/Strings/Hard/3_repeatedStringMatch.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int repeatedStringMatch(String a, String b) {
+    public int repeatedStringMatchBrute(String a, String b) {
         StringBuilder repeated = new StringBuilder();
         int count = 0;
 
@@ -15,6 +15,28 @@ class Solution {
         repeated.append(a);
         if(repeated.indexOf(b) != -1){
             return count+1;
+        }
+
+        return -1;
+    }
+
+    public int repeatedStringMatch(String a, String b) {
+        int requiredRepeats = (b.length() + a.length() - 1) / a.length();
+
+        StringBuilder repeated = new StringBuilder();
+
+        for (int i = 0; i < requiredRepeats; i++){
+            repeated.append(a);
+
+        }
+
+        if(repeated.indexOf(b) != -1){
+            return requiredRepeats;
+        }
+
+        repeated.append(a);
+        if(repeated.indexOf(b) != -1){
+            return requiredRepeats+1;
         }
 
         return -1;
