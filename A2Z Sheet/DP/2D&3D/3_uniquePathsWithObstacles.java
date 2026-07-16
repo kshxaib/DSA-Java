88eeb9ae440844cbdf5db86fe4c4365c9f42d587
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-16 12:36:13 +0530

    16 July

--- A2Z Sheet/DP/2D&3D/3_uniquePathsWithObstacles.java
+++ A2Z Sheet/DP/2D&3D/3_uniquePathsWithObstacles.java
@@ -29,3 +29,78 @@ class Solution1 {
 }
 
 
+class Solution2 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        int[][] dp = new int[m][n];
+
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+
+                // Obstacle
+                if (obstacleGrid[row][col] == 1) {
+                    dp[row][col] = 0;
+                    continue;
+                }
+
+                // Starting cell
+                if (row == 0 && col == 0) {
+                    dp[row][col] = 1;
+                    continue;
+                }
+
+                int up = 0;
+                int left = 0;
+
+                if (row > 0) up = dp[row - 1][col];
+
+                if (col > 0) left = dp[row][col - 1];
+                dp[row][col] = up + left;
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
+
+
+class Solution3 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        int[] prev = new int[n];
+
+        for (int row = 0; row < m; row++) {
+            int[] curr = new int[n];
+
+            for (int col = 0; col < n; col++) {
+                // Obstacle
+                if (obstacleGrid[row][col] == 1) {
+                    curr[col] = 0;
+                    continue;
+                }
+
+                // Starting cell
+                if (row == 0 && col == 0) {
+                    curr[col] = 1;
+                    continue;
+                }
+
+                int up = 0;
+                int left = 0;
+
+                if (row > 0) up = prev[col];
+
+                if (col > 0) left = curr[col - 1];
+                curr[col] = up + left;
+            }
+
+            prev = curr;
+        }
+
+        return prev[n - 1];
+    }
+}
