2f4a09b21bfd2b5f1ef3501dd6ffdbfc95bf64c2
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-17 13:04:46 +0530

    17 July

--- A2Z Sheet/DP/Subsequences/7_findTargetSumWays.java
+++ A2Z Sheet/DP/Subsequences/7_findTargetSumWays.java
@@ -41,3 +41,87 @@ class Solution1 {
         return dp[index][target] = take + notTake;
     }
 }
+
+
+
+class Solution2 {
+    public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
+
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        if (totalSum - target < 0 || (totalSum - target) % 2 != 0) return 0;
+
+        int sum = (totalSum - target) / 2;
+
+        int[][] dp = new int[n][sum + 1];
+
+        // Base case
+        if (nums[0] == 0) dp[0][0] = 2;
+        else dp[0][0] = 1;
+
+        if (nums[0] != 0 && nums[0] <= sum) dp[0][nums[0]] = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int s = 0; s <= sum; s++) {
+                int notTake = dp[i - 1][s];
+
+                int take = 0;
+                if (nums[i] <= s) {
+                    take = dp[i - 1][s - nums[i]];
+                }
+
+                dp[i][s] = take + notTake;
+            }
+        }
+
+        return dp[n - 1][sum];
+    }
+}
+
+
+
+class Solution3 {
+    public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
+
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        if (totalSum - target < 0 || (totalSum - target) % 2 != 0) return 0;
+
+        int sum = (totalSum - target) / 2;
+
+        int[] prev = new int[sum + 1];
+
+        // Base case
+        if (nums[0] == 0) prev[0] = 2;
+        else prev[0] = 1;
+
+        if (nums[0] != 0 && nums[0] <= sum) prev[nums[0]] = 1;
+
+        for (int i = 1; i < n; i++) {
+            int[] curr = new int[sum + 1];
+
+            for (int s = 0; s <= sum; s++) {
+                int notTake = prev[s];
+
+                int take = 0;
+                if (nums[i] <= s) {
+                    take = prev[s - nums[i]];
+                }
+
+                curr[s] = take + notTake;
+            }
+
+            prev = curr;
+        }
+
+        return prev[sum];
+    }
+}
