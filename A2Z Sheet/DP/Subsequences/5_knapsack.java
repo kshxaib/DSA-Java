2d7739422e29f9894f744624d32617eeaabcc7f9
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-17 11:41:44 +0530

    17 July

--- A2Z Sheet/DP/Subsequences/5_knapsack.java
+++ A2Z Sheet/DP/Subsequences/5_knapsack.java
@@ -31,3 +31,61 @@ class Solution1 {
         return dp[index][capacity] = Math.max(take, notTake);
     }
 }
+
+
+
+class Solution2 {
+    public int knapsack(int[] wt, int[] val, int W) {
+        int n = wt.length;
+
+        int[][] dp = new int[n][W + 1];
+
+        // Base case
+        for (int capacity = wt[0]; capacity <= W; capacity++) {
+            dp[0][capacity] = val[0];
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int capacity = 0; capacity <= W; capacity++) {
+
+                int notTake = dp[i - 1][capacity];
+
+                int take = 0;
+                if (wt[i] <= capacity) {
+                    take = val[i] + dp[i - 1][capacity - wt[i]];
+                }
+
+                dp[i][capacity] = Math.max(take, notTake);
+            }
+        }
+
+        return dp[n - 1][W];
+    }
+}
+
+
+
+class Solution3 {
+
+    public int knapsack(int[] wt, int[] val, int W) {
+        int n = wt.length;
+
+        int[] dp = new int[W + 1];
+
+        // Base case
+        for (int capacity = wt[0]; capacity <= W; capacity++) {
+            dp[capacity] = val[0];
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int capacity = W; capacity >= 0; capacity--) {
+
+                if (wt[i] <= capacity) {
+                    dp[capacity] = Math.max(dp[capacity], val[i] + dp[capacity - wt[i]]);
+                }
+            }
+        }
+
+        return dp[W];
+    }
+}
