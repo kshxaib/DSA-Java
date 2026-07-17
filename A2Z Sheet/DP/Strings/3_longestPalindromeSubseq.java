1e6d9e7057b1b2306db6c4fa22c5167da5f54769
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-17 17:40:26 +0530

    17 July

--- A2Z Sheet/DP/Strings/3_longestPalindromeSubseq.java
+++ A2Z Sheet/DP/Strings/3_longestPalindromeSubseq.java
@@ -53,3 +53,30 @@ class Solution2 {
     }
 }
 
+
+
+class Solution3 {
+    public int longestPalindromeSubseq(String s) {
+        String rev = new StringBuilder(s).reverse().toString();
+
+        int n = s.length();
+
+        int[] prev = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            int[] curr = new int[n + 1];
+
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == rev.charAt(j - 1)) {
+                    curr[j] = 1 + prev[j - 1];
+                } else {
+                    curr[j] = Math.max(prev[j], curr[j - 1]);
+                }
+            }
+
+            prev = curr;
+        }
+
+        return prev[n];
+    }
+}
