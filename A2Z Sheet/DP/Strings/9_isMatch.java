98a4f3d2bb93b7413662e6569adae1014ab8974c
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-17 22:18:47 +0530

    17 July

--- A2Z Sheet/DP/Strings/9_isMatch.java
+++ A2Z Sheet/DP/Strings/9_isMatch.java
@@ -45,3 +45,48 @@ class Solution1 {
         return ans;
     }
 }
+
+
+class Solution2 {
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+
+        boolean[][] dp = new boolean[n + 1][m + 1];
+
+        dp[0][0] = true;
+
+        for (int j = 1; j <= m; j++) {
+            boolean flag = true;
+
+            for (int k = 1; k <= j; k++) {
+
+                if (p.charAt(k - 1) != '*') {
+                    flag = false;
+                    break;
+                }
+            }
+
+            dp[0][j] = flag;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+
+                } else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
+
+
