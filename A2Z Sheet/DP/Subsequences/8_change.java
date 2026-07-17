7ff452da2611e20570be20868f1dd3c5f6a04ab2
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-17 14:03:35 +0530

    17 July

--- A2Z Sheet/DP/Subsequences/8_change.java
+++ A2Z Sheet/DP/Subsequences/8_change.java
@@ -32,3 +32,35 @@ class Solution1 {
     }
 }
 
+
+
+class Solution2 {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+
+        int[][] dp = new int[n][amount + 1];
+
+        // Base case
+        for (int target = 0; target <= amount; target++) {
+            if (target % coins[0] == 0) dp[0][target] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int target = 0; target <= amount; target++) {
+
+                int notTake = dp[i - 1][target];
+
+                int take = 0;
+                if (coins[i] <= target) {
+                    take = dp[i][target - coins[i]];
+                }
+
+                dp[i][target] = take + notTake;
+            }
+        }
+
+        return dp[n - 1][amount];
+    }
+}
+
+
