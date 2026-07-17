b286df9a9072c39abe6de39565e9956557c65ae0
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-17 22:17:22 +0530

    17 July

---
+++ A2Z Sheet/DP/Strings/9_isMatch.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution1 {
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+
+        int[][] dp = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        return solve(n - 1, m - 1, s, p, dp);
+    }
+
+    private boolean solve(int i, int j, String s, String p, int[][] dp) {
+        if (i < 0 && j < 0) return true;
+
+        if (j < 0) return false;
+
+        if (i < 0) {
+            for (int k = 0; k <= j; k++) {
+                if (p.charAt(k) != '*')
+                    return false;
+            }
+            return true;
+        }
+
+        if (dp[i][j] != -1) return dp[i][j] == 1;
+
+        boolean ans;
+
+        if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?') {
+            ans = solve(i - 1, j - 1, s, p, dp);
+
+        } else if (p.charAt(j) == '*') {
+            ans = solve(i - 1, j, s, p, dp) || solve(i, j - 1, s, p, dp);
+
+        } else {
+            ans = false;
+        }
+
+        dp[i][j] = ans ? 1 : 0;
+        return ans;
+    }
+}
