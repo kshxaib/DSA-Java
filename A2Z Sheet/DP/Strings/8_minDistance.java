c8fc233aacb9c8e8d9d1622bd6b12e0b2419ce60
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-17 20:57:32 +0530

    17 July

--- A2Z Sheet/DP/Strings/8_minDistance.java
+++ A2Z Sheet/DP/Strings/8_minDistance.java
@@ -69,3 +69,36 @@ class Solution2 {
 }
 
 
+class Solution3 {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        int[] prev = new int[m + 1];
+
+        for (int j = 0; j <= m; j++) {
+            prev[j] = j;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            int[] curr = new int[m + 1];
+            curr[0] = i;
+
+            for (int j = 1; j <= m; j++) {
+
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    curr[j] = prev[j - 1];
+                } else {
+                    int insert = 1 + curr[j - 1];
+                    int delete = 1 + prev[j];
+                    int replace = 1 + prev[j - 1];
+                    curr[j] = Math.min(insert, Math.min(delete, replace));
+                }
+            }
+
+            prev = curr;
+        }
+
+        return prev[m];
+    }
+}
