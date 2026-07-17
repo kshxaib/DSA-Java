5526cb52bf358b1795abbd400606ce47ded2be6c
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-17 17:21:13 +0530

    17 July

--- A2Z Sheet/DP/Strings/2_longestCommonSubstr.java
+++ A2Z Sheet/DP/Strings/2_longestCommonSubstr.java
@@ -25,3 +25,32 @@ class Solution2 {
     }
 }
 
+
+
+class Solution3 {
+    public int longestCommonSubstr(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+
+        int[] prev = new int[m + 1];
+
+        int maxLength = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int[] curr = new int[m + 1];
+
+            for (int j = 1; j <= m; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    curr[j] = 1 + prev[j - 1];
+                    maxLength = Math.max(maxLength, curr[j]);
+                } else {
+                    curr[j] = 0;
+                }
+            }
+
+            prev = curr;
+        }
+
+        return maxLength;
+    }
+}
