c9bc2cae590e91ac8527cf367102b9511248ae16
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-17 20:33:51 +0530

    17 July

--- A2Z Sheet/DP/Strings/7_numDistinct.java
+++ A2Z Sheet/DP/Strings/7_numDistinct.java
@@ -23,10 +23,67 @@ class Solution1 {
         if (s.charAt(i) == t.charAt(j)) {
             int take = solve(i - 1, j - 1, s, t, dp);
             int notTake = solve(i - 1, j, s, t, dp);
-            
+
             return dp[i][j] = take + notTake;
         }
 
         return dp[i][j] = solve(i - 1, j, s, t, dp);
+    }
+}
+
+
+class Solution2 {
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        // Empty target can always be formed
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
+
+
+class Solution3 {
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+
+        int[] prev = new int[m + 1];
+        prev[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            int[] curr = new int[m + 1];
+            curr[0] = 1;
+
+            for (int j = 1; j <= m; j++) {
+                
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    curr[j] = prev[j - 1] + prev[j];
+                } else {
+                    curr[j] = prev[j];
+                }
+            }
+
+            prev = curr;
+        }
+
+        return prev[m];
     }
 }
