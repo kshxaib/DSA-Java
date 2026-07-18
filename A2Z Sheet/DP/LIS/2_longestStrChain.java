9f9534a1096028194b0e672056445cf579a8e086
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-18 17:25:12 +0530

    18 July

---
+++ A2Z Sheet/DP/LIS/2_longestStrChain.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public int longestStrChain(String[] words) {
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+
+        int n = words.length;
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        int maxLength = 1;
+
+        for (int index = 0; index < n; index++) {
+            for (int prevIndex = 0; prevIndex < index; prevIndex++) {
+
+                if (isPredecessor(words[prevIndex], words[index]) && dp[prevIndex] + 1 > dp[index]) {
+                    dp[index] = dp[prevIndex] + 1;
+                }
+            }
+
+            maxLength = Math.max(maxLength, dp[index]);
+        }
+
+        return maxLength;
+    }
+
+    private boolean isPredecessor(String shorter, String longer) {
+        if (longer.length() != shorter.length() + 1) return false;
+
+        int i = 0;
+        int j = 0;
+
+        while (i < shorter.length() && j < longer.length()) {
+            if (shorter.charAt(i) == longer.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+
+        return i == shorter.length();
+    }
+}
