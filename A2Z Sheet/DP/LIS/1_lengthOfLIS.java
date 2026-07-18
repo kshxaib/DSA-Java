63ab5eae44def884c03cc0468ba0dca47d065d8d
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-18 13:53:17 +0530

    18 July

---
+++ A2Z Sheet/DP/LIS/1_lengthOfLIS.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution1 {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+
+        int[][] dp = new int[n][n + 1];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        return solve(0, -1, nums, dp);
+    }
+
+    private int solve(int index, int prevIndex, int[] nums, int[][] dp) {
+        if (index == nums.length) return 0;
+
+        if (dp[index][prevIndex + 1] != -1) return dp[index][prevIndex + 1];
+
+        int notTake = solve(index + 1, prevIndex, nums, dp);
+
+        int take = 0;
+        if (prevIndex == -1 || nums[index] > nums[prevIndex]) {
+            take = 1 + solve(index + 1, index, nums, dp);
+        }
+
+        return dp[index][prevIndex + 1] = Math.max(take, notTake);
+    }
+}
