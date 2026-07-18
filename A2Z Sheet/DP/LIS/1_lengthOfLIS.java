029b6e06fbdd6baf38326fb23074535a02ca24c1
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-18 15:10:53 +0530

    18 July

--- A2Z Sheet/DP/LIS/1_lengthOfLIS.java
+++ A2Z Sheet/DP/LIS/1_lengthOfLIS.java
@@ -84,3 +84,29 @@ class Solution3 {
     }
 }
 
+
+
+class Solution4 {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+
+        int[] dp = new int[n];
+
+        Arrays.fill(dp, 1);
+
+        int maxLength = 1;
+
+        for (int index = 0; index < n; index++) {
+            for (int prevIndex = 0; prevIndex < index; prevIndex++) {
+
+                if (nums[index] > nums[prevIndex]) {
+                    dp[index] = Math.max(dp[index], 1 + dp[prevIndex]);
+                }
+            }
+
+            maxLength = Math.max(maxLength, dp[index]);
+        }
+
+        return maxLength;
+    }
+}
