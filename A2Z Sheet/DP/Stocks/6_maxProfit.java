7e4efcfc024a201aff1227014fad7626c357124a
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-18 12:49:21 +0530

    18 July

--- A2Z Sheet/DP/Stocks/6_maxProfit.java
+++ A2Z Sheet/DP/Stocks/6_maxProfit.java
@@ -34,3 +34,20 @@ class Solution1 {
         return dp[index][buy] = profit;
     }
 }
+
+
+class Solution2 {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+
+        int[][] dp = new int[n + 1][2];
+
+        for (int index = n - 1; index >= 0; index--) {
+            dp[index][1] = Math.max(-prices[index] + dp[index + 1][0], dp[index + 1][1]);
+
+            dp[index][0] = Math.max(prices[index] - fee + dp[index + 1][1], dp[index + 1][0]);
+        }
+
+        return dp[0][1];
+    }
+}
