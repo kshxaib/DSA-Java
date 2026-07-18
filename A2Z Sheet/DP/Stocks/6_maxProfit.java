50de8e3cc6e15661cb7d11096224c31e51801a06
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-18 12:48:44 +0530

    18 July

---
+++ A2Z Sheet/DP/Stocks/6_maxProfit.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution1 {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+
+        int[][] dp = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        return solve(0, 1, prices, fee, dp);
+    }
+
+    private int solve(int index, int buy, int[] prices, int fee, int[][] dp) {
+        if (index == prices.length) return 0;
+
+        if (dp[index][buy] != -1) return dp[index][buy];
+
+        int profit;
+
+        if (buy == 1) {
+            int buyStock = -prices[index] + solve(index + 1, 0, prices, fee, dp);
+            int skip = solve(index + 1, 1, prices, fee, dp);
+            profit = Math.max(buyStock, skip);
+
+        } else {
+            int sellStock = prices[index] - fee + solve(index + 1, 1, prices, fee, dp);
+            int skip = solve(index + 1, 0, prices, fee, dp);
+            profit = Math.max(sellStock, skip);
+        }
+
+        return dp[index][buy] = profit;
+    }
+}
