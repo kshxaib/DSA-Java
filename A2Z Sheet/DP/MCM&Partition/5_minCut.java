a3df09be090da60e7d6f7cdd104f0d00d92035d4
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-19 14:47:18 +0530

    19 July

---
+++ A2Z Sheet/DP/MCM&Partition/5_minCut.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int minCut(String s) {
+        int n = s.length();
+
+        int[] dp = new int[n];
+
+        Arrays.fill(dp, -1);
+
+        return solve(0, s, dp) - 1;
+    }
+
+    private int solve(int index, String s, int[] dp) {
+        if (index == s.length()) return 0;
+
+        if (dp[index] != -1) return dp[index];
+
+        int minCuts = Integer.MAX_VALUE;
+
+        for (int j = index; j < s.length(); j++) {
+
+            if (isPalindrome(index, j, s)) {
+                int cuts = 1 + solve(j + 1, s, dp);
+                minCuts = Math.min(minCuts, cuts);
+            }
+        }
+
+        return dp[index] = minCuts;
+    }
+
+    private boolean isPalindrome(int left, int right, String s) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
