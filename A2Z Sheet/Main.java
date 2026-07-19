39b4458e689af390a5c67becfecb34a9704e8247
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-19 19:13:30 +0530

    19 July

--- A2Z Sheet/Main.java
+++ A2Z Sheet/Main.java
@@ -1,35 +1,5 @@
-class Solution {
-    public int fib(int n) {
-        // If n is 0 return 0
-        if(n == 0) return 0;
-        // If n is 1 return 1
-        if(n == 1) return 1;
 
-        // prev2 stores fib(n-2)
-        int prev2 = 0;
-        // prev stores fib(n-1)
-        int prev = 1;
-        // curr stores current fib
-        int curr = 0;
-
-        // Loop from 2 to n
-        for(int i = 2; i <= n; i++) {
-            // Calculate current fib
-            curr = prev + prev2;
-            // Update prev2
-            prev2 = prev;
-            // Update prev
-            prev = curr;
-        }
-        // Return final answer
-        return prev;
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        int n = 10;
-        System.out.println(s.fib(n));
-    }
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isEnd;
 }
