7ea64e77e84ef7ed0c04d136c89593cbf4576172
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-19 15:06:09 +0530

    19 July

--- A2Z Sheet/DP/MCM&Partition/5_minCut.java
+++ A2Z Sheet/DP/MCM&Partition/5_minCut.java
@@ -4,25 +4,26 @@ class Solution1 {
     public int minCut(String s) {
         int n = s.length();
 
-        int[] dp = new int[n];
+        int[] dp = new int[n];  // dp[i] = minimum partitions from index i  
 
         Arrays.fill(dp, -1);
 
-        return solve(0, s, dp) - 1;
+        return solve(0, s, dp) - 1; // partitions - 1 = cuts
     }
 
     private int solve(int index, String s, int[] dp) {
-        if (index == s.length()) return 0;
+        if (index == s.length()) return 0;   // entire string processed
 
         if (dp[index] != -1) return dp[index];
 
         int minCuts = Integer.MAX_VALUE;
 
+        // try every ending index
         for (int j = index; j < s.length(); j++) {
 
             if (isPalindrome(index, j, s)) {
-                int cuts = 1 + solve(j + 1, s, dp);
-                minCuts = Math.min(minCuts, cuts);
+                int cuts = 1 + solve(j + 1, s, dp); // current partition + remaining partitions
+                minCuts = Math.min(minCuts, cuts);  // keep minimum
             }
         }
 
