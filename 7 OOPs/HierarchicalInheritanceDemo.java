752169958397eb471a462eaa8129d5feb5941d7d
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-25 23:47:44 +0530

    25 July

---
+++ 7 OOPs/HierarchicalInheritanceDemo.java
@@ -0,0 +1,34 @@
+class Animal {
+    void eat() {
+        System.out.println("Animal eats");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    void meow() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class HierarchicalInheritanceDemo {
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        Cat c = new Cat();
+
+        d.eat();
+        d.bark();
+
+        c.eat();
+        c.meow();
+    }
+}
+
+
+
