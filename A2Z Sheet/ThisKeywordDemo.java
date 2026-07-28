315a2c976fda5c2ff36edc6f05f5b668f92617ff
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-28 22:26:21 +0530

    28 July

--- A2Z Sheet/ThisKeywordDemo.java
+++ A2Z Sheet/ThisKeywordDemo.java
@@ -1,26 +1,16 @@
-class Student {
-    String name;
-    int age;
+class A{
 
-    Student(String name, int age) {
-
-        this.name = name;
-        this.age = age;
-    }
-
-    void display() {
-
-        System.out.println("Name : " + this.name);
-        System.out.println("Age : " + this.age);
-    }
 }
 
-public class ThisKeywordDemo {
-    public static void main(String[] args) {
+class B extends A{
 
-        Student s = new Student("Khan Shoaib", 20);
-
-        s.display();
-    }
 }
 
+class C extends A{
+
+}
+
+// Not Allowed
+class D extends B, C{
+
+}
