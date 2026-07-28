e2447d7101fe512efd1e979cb34bf37f6287259b
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-28 22:53:06 +0530

    28 July

--- 7 OOPs/Main.java
+++ 7 OOPs/Main.java
@@ -1,25 +1,13 @@
 class Student {
-    static String college = "ABC College";
-    String name;
-
-    Student(String name) {
-        this.name = name;
-    }
-
-    void display() {
-        System.out.println(name + " - " + college);
+    static void show() {
+        System.out.println("Welcome to Java");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
 
-        Student s1 = new Student("Shoaib");
-        Student s2 = new Student("Faisal");
-
-        s1.display();
-        s2.display();
+        Student.show();
     }
 }
-
 
