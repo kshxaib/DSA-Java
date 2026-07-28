246f93fb6355629f127ba050f9e0be82828a558b
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-28 23:36:16 +0530

    28 July

--- 7 OOPs/Main.java
+++ 7 OOPs/Main.java
@@ -1,9 +1,19 @@
-class Test {
-    public static void main(String[] args) {
-        Test t1 = new Test();
-        Test t2 = t1;
-        System.out.println(t1 == t2);
+class Parent {
+    void show() {
+        System.out.println("Parent");
     }
 }
 
+class Child extends Parent {
+    void show() {
+        System.out.println("Child");
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Parent p = new Child();
+        p.show();
+    }
+}
 
