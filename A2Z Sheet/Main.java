54a4c89579d7b6164795637a05d55cb1ffe5e8cc
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-07-28 22:29:41 +0530

    28 July

---
+++ A2Z Sheet/Main.java
@@ -0,0 +1,34 @@
+interface Camera{
+
+    void click();
+}
+
+interface MusicPlayer{
+
+    void playMusic();
+}
+
+class SmartPhone implements Camera, MusicPlayer{
+
+    public void click(){
+
+        System.out.println("Photo Clicked");
+    }
+
+    public void playMusic(){
+
+        System.out.println("Playing Music");
+    }
+}
+
+public class Main{
+
+    public static void main(String[] args){
+
+        SmartPhone phone = new SmartPhone();
+
+        phone.click();
+
+        phone.playMusic();
+    }
+}
